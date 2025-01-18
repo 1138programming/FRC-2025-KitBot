6cb6ccf931f2f002b0b93c986ac3c283597c95b5
Glutinfree <117413359+JAKlein26@users.noreply.github.com>
2025-01-17 16:56:35 -0800

    Started Roller for Coral

--- FRC-2025-KitBot/src/main/java/frc/robot/Constants.java
+++ FRC-2025-KitBot/src/main/java/frc/robot/Constants.java
@@ -1,5 +1,12 @@
 package frc.robot;
 
 public class Constants {
-    
+    public static final class CoralReleaseConstants 
+    {
+        public static final int KRollerMotorID = 1; //ID
+        public static final int KRollerMotorLimit = 80; //So it won't over heat
+        public static final double KRollerMotorVoltage = 50; // In sync with battery
+        
+        //All random numbers!
+    }
 }
